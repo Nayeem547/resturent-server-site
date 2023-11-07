d507f4a6f1d483f1fff77ac0f54134c05ab5fa07
Nayeem547 <nayennh547@gmail.com>
2023-11-07 07:19:52 +0600

    showing data dynamically for get the data from database

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nprvrf8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,6 +45,13 @@ async function run() {
     app.get('/productsCount', async(req, res) => {
         const count = await foodCollection.estimatedDocumentCount();
         res.send({count});
+      })
+
+      app.get('/allfoods/details/:id', async(req, res) => {
+        const id = req.params.id;
+        const queary = {_id:  new ObjectId(id)}
+        const result = await foodCollection.findOne(queary);
+        res.send(result);
       })
 
 
