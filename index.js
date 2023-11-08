878cae6e1615527b8aa6516aae5e60cd56baab63
Nayeem547 <nayennh547@gmail.com>
2023-11-08 22:21:47 +0600

    jwt token steps completed

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
+
+
 const express = require('express');
 const cors = require('cors');
+
+
+const jwt = require('jsonwebtoken');
+const cookieparser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+    origin:[
+      'http://localhost:5174'
+    ],
+    credentials: true
+  }));
+  app.use(express.json()); 
+  app.use(cookieparser());
 
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nprvrf8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +33,26 @@ const client = new MongoClient(uri, {
   }
 });
 
+const logger = (req, res,next) => {
+    console.log( 'log: info', req.method, req.url);
+    next();
+  }
+  
+  const verifyToken = (req, res, next) => {
+    const Token = req?.cookies?.Token;
+    console.log('token in the middleware', Token);
+    if(!Token){
+      return res.status(401).send({message: 'unothorized'})
+    }
+    jwt.verify(Token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if(err){
+        return res.send({message : 'unauthorized access'})
+      }
+      req.user = decoded;
+    })
+    next();
+  }
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +61,25 @@ async function run() {
     const foodCollection = client.db('ResturentDB').collection('allfoods');
     const userCollection = client.db('ResturentDB').collection('user');
     const PurchaseCollection = client.db("ResturentDB").collection("userStore");
+
+
+    app.post('/jwt', async(req, res)=> {
+        const user = req.body;
+        console.log('user for token', user);
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h' });
+        res.cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+        })
+        .send({ success: true });
+      });
+
+       app.post('/logout', async(req, res) => {
+        const user = req.body;
+        console.log('logging out', user);
+        res.clearCookie('token', {maxAge: 0}).send({success: true});
+       })
 
     app.post('/user', async(req, res)=> {
         const user = req.body;
