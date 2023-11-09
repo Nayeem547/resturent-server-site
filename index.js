56b7492c95e6ca6038146703f68c7cb2bac392d2
Nayeem547 <nayennh547@gmail.com>
2023-11-09 19:31:48 +0600

    some issue updated

--- index.js
+++ index.js
@@ -4,22 +4,15 @@ const express = require('express');
 const cors = require('cors');
 
 
-const jwt = require('jsonwebtoken');
-const cookieparser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({
-    origin:[
-      'http://localhost:5174'
-    ],
-    credentials: true
-  }));
+app.use(cors());
   app.use(express.json()); 
-  app.use(cookieparser());
+ 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nprvrf8.mongodb.net/?retryWrites=true&w=majority`;
@@ -33,53 +26,19 @@ const client = new MongoClient(uri, {
   }
 });
 
-const logger = (req, res,next) => {
-    console.log( 'log: info', req.method, req.url);
-    next();
-  }
-  
-  const verifyToken = (req, res, next) => {
-    const Token = req?.cookies?.Token;
-    console.log('token in the middleware', Token);
-    if(!Token){
-      return res.status(401).send({message: 'unothorized'})
-    }
-    jwt.verify(Token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if(err){
-        return res.send({message : 'unauthorized access'})
-      }
-      req.user = decoded;
-    })
-    next();
-  }
+
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const foodCollection = client.db('ResturentDB').collection('allfoods');
     const userCollection = client.db('ResturentDB').collection('user');
     const PurchaseCollection = client.db("ResturentDB").collection("userStore");
 
 
-    app.post('/jwt', async(req, res)=> {
-        const user = req.body;
-        console.log('user for token', user);
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h' });
-        res.cookie('token', token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: 'none'
-        })
-        .send({ success: true });
-      });
-
-      app.post('/logout', async(req, res) => {
-        const user = req.body;
-        console.log('logging out', user);
-        res.clearCookie('token', {maxAge: 0}).send({success: true});
-       });
+ 
 
     app.post('/user', async(req, res)=> {
         const user = req.body;
@@ -88,7 +47,7 @@ async function run() {
         res.send(result);
       })
   
-      app.get('/user/:email', async(req, res) => {
+      app.get('/user/:email',  async(req, res) => {
         const email = req.params.email;
         const queary = {email: email}
         const result = await userCollection.findOne(queary);
@@ -116,7 +75,7 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/allfoods', async(req, res) => {
+    app.get('/allfoods',  async(req, res) => {
         const page = parseInt(req.query.page);
         const size = parseInt(req.query.size);
 
@@ -160,10 +119,23 @@ async function run() {
         res.send(result);
       })
 
+
+      app.get("/allfoods/:Food_Name", async (req, res) => {
+        const Food_Name = req.params.Food_Name;
+        let query = { Food: Food_Name }; 
+        const cartCategory = cartCategoryCollection.find(query);
+        const result = await cartCategory.toArray();
+        res.send(result);
+      });
+
+
+
+
       app.put('/allfoods/:id', async(req, res) => {
         const id = req.params.id;
         const filter = {_id: new ObjectId(id)}
         const options = {upsert: true};
+        
         const UpdateCart = req.body;
         const carte = {
           $set: {
@@ -171,12 +143,14 @@ async function run() {
           image:  UpdateCart.Food_Image,
           Brand:  UpdateCart.Food_Category,
           Origin: UpdateCart.Food_Origin,
-          Quantity: UpdateCart.Quantity,
+          Quantity: Quantity - 1,
+          Count: 1,
           Price:  UpdateCart.Price, 
           Description:  UpdateCart.Description,
           }
         }
-        const result = await foodCollection.updateOne(filter, carte, options)
+       
+        const result = await foodCollection.updateOne(filter, carte, updatedFood, options)
         res.send(result);
       })
   
@@ -200,6 +174,6 @@ app.get('/', (req, res) =>{
 })
 
 app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
+    console.log(` server is running on port: ${port}`);
 })
 
